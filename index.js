a1bbfaf9e646aada6428485390a2d8c4f3827805
klaudiabilska <klaudiabilska1@gmail.com>
2023-11-29 21:45:05 +0100

    commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ canvas.height = innerHeight
 
 // boundries
 class Boundary {
+    static width = 40
+    static height = 40
     constructor({position}) {
         this.position = position
         this.width = 40
@@ -19,20 +21,30 @@ class Boundary {
     }
 }
 
-const boundaries = [
-    new Boundary({
-        position: {
-            x: 0,
-            y: 0
-        }
-    }), 
-    new Boundary({
-        position: {
-            x: 41,
-            y: 0
+const map = [
+    ['-', '-', '-', '-', '-', '-'], 
+    ['-', ' ', ' ', ' ', ' ', '-'], 
+    ['-', ' ', '-', '-', ' ', '-'], 
+    ['-', ' ', ' ', ' ', ' ', '-'], 
+    ['-', '-', '-', '-', '-', '-']
+]
+const boundaries = []
+
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+            case '-':
+                boundaries.push(new Boundary({
+                    position: {
+                        x: Boundary.width  * j,
+                        y: Boundary.height  * i
+                    }
+                })
+                )
+                break
         }
     })
-]
+})
 
 boundaries.forEach(boundary => {
     boundary.draw()
